7ca34439e8df73539748aa36cdddcda28ce7d4fc
olexandr <krizhanovsky@softpro.ua>
2025-02-12 21:26:13 +0200

    1

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,8 +1,54 @@
 import { Task } from "../models/task.js";
 
 export const addOneTask = async (req, res, next) => {
-  const { _id } = req.user;
+  try {
+    const { _id } = req.user;
+    const task = await Task.create({ ...req.body, creater: _id });
+    res.status(201).json(task);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
-  const task = await Task.create({ ...req.body, creater: _id });
-  res.status(201).json(task);
+export const getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params?.id);
+    res.status(200).json(task);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ message: "Server Error" });
+  }
+};
+
+export const editTask = async (req, res, next) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params?.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(task);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const deleteTask = async (req, res, next) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const getTasksByUser = async (req, res, next) => {
+  try {
+    const tasks = await Task.find({ users: { $in: [req.body.userId] } });
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -1,9 +1,23 @@
 import express from "express";
 import { authenticate } from "../middlewares/authenticate.js";
-import { addOneTask } from "../controllers/taskController.js";
+import {
+  addOneTask,
+  deleteTask,
+  editTask,
+  getTaskById,
+  getTasksByUser,
+} from "../controllers/taskController.js";
 
 const tasksRouter = express.Router();
 
+tasksRouter.get("", authenticate, getTasksByUser);
+
 tasksRouter.post("", authenticate, addOneTask);
+
+tasksRouter.put("/:id", authenticate, editTask);
+
+tasksRouter.get("/:id", authenticate, getTaskById);
+
+tasksRouter.delete("/:id", authenticate, deleteTask);
 
 export default tasksRouter;
